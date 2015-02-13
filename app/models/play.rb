31c01c6a73ed0f5ac43edcfd976097bcdab66820
Dmitry Chopey <Dmytro.Chopei@hneu.net>
2015-02-13 13:04:47 +0200

    Add dependent: :destroy in Play model

--- app/models/play.rb
+++ app/models/play.rb
@@ -1,6 +1,6 @@
 class Play < ActiveRecord::Base
   has_many :roles, dependent: :destroy
-  has_many :performances
+  has_many :performances, dependent: :destroy
   accepts_nested_attributes_for :roles, allow_destroy: true
   validates_presence_of :title, :description, :author, :year_of_foundation
 end
